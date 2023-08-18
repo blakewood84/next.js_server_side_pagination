73f2c14ebf1a440f5e72fbdc2efcb402b0cc4d66
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-08-18 15:42:02 -0700

    Added simple pagination.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { fetchCurated } from "@/lib/fetchCurated";
 import Image from "next/image";
+import Link from "next/link";
 
+// Pages are per_page / total
 export default async function Home({
   searchParams,
 }: {
@@ -8,14 +10,14 @@ export default async function Home({
 }) {
   const page = typeof searchParams.page === "string" ? searchParams.page : "1";
   const result = (await fetchCurated({ page })).photos;
-  const timeout = await new Promise((resolve) => setTimeout(resolve, 3000));
+  console.log("result: ", result);
   return (
     <main className="container mx-auto">
       <div className="p-10">Hello</div>
       <div className="flex flex-wrap">
         {result.map((photo, index) => {
           return (
-            <div className="relative w-[300px] h-[200px]">
+            <div className="relative w-[300px] h-[200px]" key={index}>
               <Image
                 className="object-cover"
                 src={photo.src.landscape}
@@ -25,6 +27,20 @@ export default async function Home({
             </div>
           );
         })}
+      </div>
+      <div className="w-full flex justify-center gap-4 pt-10">
+        <Link
+          href={`?page=${Number(page) + 1}`}
+          className="border rounded px-4 py-1 cursor-pointer"
+        >
+          Next
+        </Link>
+        <Link
+          href={Number(page) > 2 ? `?page=${Number(page) - 1}` : "/"}
+          className="border rounded px-4 py-1 cursor-pointer"
+        >
+          Previous
+        </Link>
       </div>
     </main>
   );
